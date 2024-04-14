69d55ef05c60e075df69b5785c37a2abf4603457
unknown <isabqm121@gmail.com>
2024-04-14 19:34:11 -0400

    Implementación de movimientos R y R'

--- Cubo Rubik/main.py
+++ Cubo Rubik/main.py
@@ -81,7 +81,6 @@ class Rubik_cube():
         line_number=[-1,0,0,-1]
         self.row_and_column_motion(sides,line_number)
     
-    
     def back_clockwise(self):
         sides=[0,3,5,1]
         line_number=[0,-1,-1,0]
@@ -94,7 +93,6 @@ class Rubik_cube():
         
     def row_and_column_motion(self,sides,line_number):
         first_line=(self.sides[sides[3]][:,line_number[-1]]).copy()
-        print("Primera línea:",first_line)
         for position in range(len(sides)-1):
             if position%2==0: #si es fila
                 line=self.sides[sides[position]][line_number[position]]
@@ -102,8 +100,23 @@ class Rubik_cube():
             else:#si es columna
                 line=self.sides[sides[position]][:,line_number[position]]
                 self.sides[sides[position-1]][line_number[position-1]]=line
-        print("Primera línea:",first_line)
         self.sides[sides[2]][line_number[2]]=first_line
+
+    def right_clockwise(self):
+        sides=[0,2,5,4]
+        columns=[-1,-1,-1,0]
+        self.column_motion(sides,columns)
+    
+    def right_inverted(self):
+        sides=[0,4,5,2]
+        columns=[-1,0,-1,-1]
+        self.column_motion(sides,columns)
+    def column_motion(self,sides, columns):
+        first_line=self.sides[sides[-1]][:,columns[-1]].copy()
+        for position in range(len(sides)-1):
+            line=self.sides[sides[position]][:,columns[position]]
+            self.sides[sides[position-1]][:,columns[position-1]]=line
+        self.sides[sides[2]][:,columns[2]]=first_line
 
 if __name__ == "__main__":
     rubik=Rubik_cube()
@@ -118,7 +131,9 @@ if __name__ == "__main__":
     # rubik.front_clockwise()
     # rubik.front_inverted()
     # rubik.back_clockwise()
-    rubik.back_inverted()
+    # rubik.back_inverted()
+    # rubik.right_clockwise()
+    rubik.right_inverted()
     rubik.show_cube()
 
 
