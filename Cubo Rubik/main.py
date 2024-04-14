2f7f55b3b7b5c32acbc31da8cdbbe0df4e9a91e3
unknown <isabqm121@gmail.com>
2024-04-13 23:36:10 -0400

    Implementación de D y D'

--- Cubo Rubik/main.py
+++ Cubo Rubik/main.py
@@ -44,32 +44,42 @@ class Rubik_cube():
         for side in self.sides:
             self.print_side(side)
             print("------")
+
     def up_clockwise(self):
         sides=[1,2,3,4]
-        self.up_wise(sides,0)
-        
+        self.row_motion(sides,0)
     
     def up_inverted(self):
         sides=[4,3,2,1]
-        self.up_wise(sides,0)
+        self.row_motion(sides,0)
+    
+    def down_clockwise(self):
+        sides=[4,3,2,1]
+        self.row_motion(sides,-1)
+        
+    
+    def down_inverted(self):
+        sides=[1,2,3,4]
+        self.row_motion(sides,-1)
 
-    def up_wise(self, sides_list, row):
+    def row_motion(self, sides_list, row):
         sides=sides_list
-        first_side_line=self.sides[sides[-1]][row]
-        print(first_side_line)
+        first_line=self.sides[sides[-1]][row]
         for position in range(len(sides)-1):
-            print(position)
-            first_line=self.sides[sides[position]][row]
-            self.sides[sides[position-1]][row]=first_line
-            print(first_line)
-        self.sides[sides[2]][row]=first_side_line
+            line=self.sides[sides[position]][row]
+            self.sides[sides[position-1]][row]=line
+        self.sides[sides[2]][row]=first_line
 
 if __name__ == "__main__":
     rubik=Rubik_cube()
-    print("Up clockwise")
-    rubik.up_clockwise()
+    # print("Up clockwise")
+    # rubik.up_clockwise()
     # print("Up inverted")
     # rubik.up_inverted()
+    # print("down Clockwise")
+    # rubik.down_clockwise()
+    print("down inverted")
+    rubik.down_inverted()
     rubik.show_cube()
     
 
